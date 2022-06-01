67b202ac2963851ecc1d9b0080e95ea890f78298
AbhishekKhot <khotabhishek469@gmail.com>
2022-06-01 16:25:41 +0530

    added pieChart

--- app/src/main/java/com/example/internshiptask/fragments/SecondFragment.kt
+++ app/src/main/java/com/example/internshiptask/fragments/SecondFragment.kt
@@ -1,10 +1,13 @@
 package com.example.internshiptask.fragments
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.androidplot.pie.Segment
+import com.androidplot.pie.SegmentFormatter
 import com.example.internshiptask.databinding.FragmentSecondBinding
 
 class SecondFragment : Fragment() {
@@ -25,8 +28,16 @@ class SecondFragment : Fragment() {
         return root
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val pieChart=binding.pieChart
+
+        pieChart.addSegment(Segment("one",40), SegmentFormatter(Color.RED))
+        pieChart.addSegment(Segment("two",30), SegmentFormatter(Color.GREEN))
+        pieChart.addSegment(Segment("three",20), SegmentFormatter(Color.BLUE))
+        pieChart.addSegment(Segment("four",10), SegmentFormatter(Color.YELLOW))
     }
 
     override fun onDestroyView() {
