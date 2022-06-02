d7e2195b6ab7a644bf49b487223affb538afa4a0
AbhishekKhot <khotabhishek469@gmail.com>
2022-06-02 07:00:15 +0530

    x-y plot

--- app/src/main/java/com/example/internshiptask/fragments/XYPlotFragment.kt
+++ app/src/main/java/com/example/internshiptask/fragments/XYPlotFragment.kt
@@ -4,8 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.fragment.app.Fragment
+import com.example.internshiptask.R
 import com.example.internshiptask.databinding.FragmentXyPlotBinding
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+import com.jjoe64.graphview.GridLabelRenderer
 
 
 class XYPlotFragment : Fragment() {
@@ -18,7 +23,7 @@ class XYPlotFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
-        savedInstanceState: Bundle?
+        savedInstanceState: Bundle?,
     ): View? {
         _binding = FragmentXyPlotBinding.inflate(inflater, container, false)
         val root: View = binding.root
@@ -27,6 +32,58 @@ class XYPlotFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+
+        binding.spFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                adapterView: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long,
+            ) {
+                when (position) {
+                    0 -> setUpGraph(31)
+                    1 -> setUpGraph(28)
+                    2 -> setUpGraph(30)
+                    3 -> setUpGraph(30)
+                    4 -> setUpGraph(31)
+                    5 -> setUpGraph(30)
+                    6 -> setUpGraph(31)
+                    7 -> setUpGraph(31)
+                    8 -> setUpGraph(30)
+                    9 -> setUpGraph(31)
+                    10 -> setUpGraph(30)
+                    11 -> setUpGraph(50)
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {}
+        }
+
+    }
+
+    private fun setUpGraph(end:Int){
+        lateinit var series: LineGraphSeries<DataPoint>
+        var x : Double = 0.0;  var y : Double = 0.0
+
+        series = LineGraphSeries<DataPoint>()
+
+        for (i in 1..end)
+        {
+            x += 1.0
+            y= Math.sqrt(x)
+            series.appendData(DataPoint(x,y), true, end)
+        }
+
+        val gridLabel: GridLabelRenderer = binding.idGraphView.getGridLabelRenderer()
+        gridLabel.horizontalAxisTitle = "Days of the months"
+        gridLabel.verticalAxisTitle = "Square root of x value"
+
+        binding.idGraphView.title="Graph View"
+        binding.idGraphView.titleColor=R.color.ap_black
+        binding.idGraphView.titleTextSize= 30F
+        binding.idGraphView.addSeries(series)
     }
 
     override fun onDestroyView() {
